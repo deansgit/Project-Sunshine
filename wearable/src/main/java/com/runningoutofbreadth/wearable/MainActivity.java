21b2e3dcc97e9f61d46586b30ac477a3849ce21a
Dean Nguyen <adozendonuts@users.noreply.github.com>
2016-05-21 15:23:53 -0400

    added base template for round and rect, added digital/analog watchfaces

--- wearable/src/main/java/com/runningoutofbreadth/wearable/MainActivity.java
+++ wearable/src/main/java/com/runningoutofbreadth/wearable/MainActivity.java
@@ -8,16 +8,27 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
 
     private TextView mTextView;
+//    private FrameLayout mRectLayout;
+//    private FrameLayout mRoundLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+
         stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
             @Override
             public void onLayoutInflated(WatchViewStub stub) {
                 mTextView = (TextView) stub.findViewById(R.id.text);
+//                mRectLayout = (FrameLayout) findViewById(R.id.rect_layout);
+//                mRoundLayout = (FrameLayout) findViewById(R.id.round_layout)
+//                mTextView.setText(Utility.getFriendlyDayString(mContext, dateInMillis, useLongToday));
+//              TODO: export utility class into a library,
+//              refactor code to make references to said library,
+//              add dependencies
+//              reference Utility class and the ForecastAdapter class
+
             }
         });
     }
