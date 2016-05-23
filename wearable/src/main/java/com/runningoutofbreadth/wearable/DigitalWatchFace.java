241c9b242429b603d9d6d6890c120eb8aaf23643
Dean Nguyen <adozendonuts@users.noreply.github.com>
2016-05-23 02:46:15 -0400

    added ambient mode logic. changed background color. repositioned elements

--- wearable/src/main/java/com/runningoutofbreadth/wearable/DigitalWatchFace.java
+++ wearable/src/main/java/com/runningoutofbreadth/wearable/DigitalWatchFace.java
@@ -27,6 +27,8 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.Typeface;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -36,13 +38,14 @@ import android.support.v4.content.LocalBroadcastManager;
 import android.support.wearable.watchface.CanvasWatchFaceService;
 import android.support.wearable.watchface.WatchFaceStyle;
 import android.text.format.Time;
+import android.util.DisplayMetrics;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.SurfaceHolder;
 import android.view.WindowInsets;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.wearable.Wearable;
 
 import java.lang.ref.WeakReference;
 import java.util.TimeZone;
@@ -97,15 +100,6 @@ public class DigitalWatchFace extends CanvasWatchFaceService {
             GoogleApiClient.OnConnectionFailedListener {
         private static final String LOG_TAG = "CANVASWATCHFACE";
 
-        /**
-         * Not entirely sure why this needs to be declared here... scope?
-         */
-        GoogleApiClient mGoogleApiClient =
-                new GoogleApiClient.Builder(DigitalWatchFace.this).addApi(Wearable.API)
-                        .addConnectionCallbacks(this)
-                        .addOnConnectionFailedListener(this)
-                        .build();
-
         final Handler mUpdateTimeHandler = new EngineHandler(this);
         boolean mRegisteredTimeZoneReceiver = false;
 
@@ -117,8 +111,14 @@ public class DigitalWatchFace extends CanvasWatchFaceService {
         Paint mDatePaint;
         Paint mHighTempPaint;
         Paint mLowTempPaint;
-        Paint mWeatherIconPaint;
+        Bitmap mWeatherIconBitmap;
+//        Paint mIconHider;
 
+        /**
+         * Alpha values for ambient mode
+         */
+        static final int AMBIENT_ALPHA = 0;
+        static final int NORMAL_ALPHA = 255;
 
         boolean mAmbient;
         Time mTime;
@@ -129,8 +129,8 @@ public class DigitalWatchFace extends CanvasWatchFaceService {
                 mTime.setToNow();
             }
         };
-        int mTapCount;
-
+//        int mTapCount;
+//
         float mXOffset;
         float mYOffset;
 
@@ -156,7 +156,6 @@ public class DigitalWatchFace extends CanvasWatchFaceService {
         String mHighTemp;
         String mLowTemp;
         int mWeatherId;
-        Bitmap mWeatherIcon;
 
         @Override
         public void onCreate(SurfaceHolder holder) {
@@ -167,6 +166,7 @@ public class DigitalWatchFace extends CanvasWatchFaceService {
                     .setBackgroundVisibility(WatchFaceStyle.BACKGROUND_VISIBILITY_INTERRUPTIVE)
                     .setShowSystemUiTime(false)
                     .setAcceptsTapEvents(true)
+                    .setHotwordIndicatorGravity(Gravity.RIGHT | Gravity.BOTTOM)
                     .build());
 
             IntentFilter dataItemFilter = new IntentFilter(Intent.ACTION_SEND);
@@ -175,9 +175,10 @@ public class DigitalWatchFace extends CanvasWatchFaceService {
 
             Resources resources = DigitalWatchFace.this.getResources();
             mYOffset = resources.getDimension(R.dimen.digital_y_offset);
+            mXOffset = resources.getDimension(R.dimen.digital_x_offset);
 
             mBackgroundPaint = new Paint();
-            mBackgroundPaint.setColor(resources.getColor(R.color.background));
+            mBackgroundPaint.setColor(resources.getColor(R.color.primary_dark));
 
             mTimePaint = new Paint();
             mTimePaint = createTextPaint(resources.getColor(R.color.digital_text));
@@ -191,8 +192,9 @@ public class DigitalWatchFace extends CanvasWatchFaceService {
             mLowTempPaint = new Paint();
             mLowTempPaint = createTextPaint(resources.getColor(R.color.digital_text));
 
-            mWeatherIconPaint = new Paint();
-//            mWeatherIconPaint.set image?
+//            mIconHider = new Paint();
+//            mIconHider.setColor(resources.getColor(R.color.background));
+//            mIconHider.setAlpha(50);
 
             mTime = new Time();
         }
@@ -253,16 +255,18 @@ public class DigitalWatchFace extends CanvasWatchFaceService {
 
             // Load resources that have alternate values for round watches.
             Resources resources = DigitalWatchFace.this.getResources();
-            boolean isRound = insets.isRound();
-            mXOffset = resources.getDimension(isRound
-                    ? R.dimen.digital_x_offset_round : R.dimen.digital_x_offset);
-            float textSize = resources.getDimension(isRound
-                    ? R.dimen.digital_text_size_round : R.dimen.digital_text_size);
+//            boolean isRound = insets.isRound();
+//            mXOffset = resources.getDimension(isRound
+//                    ? R.dimen.digital_x_offset_round : R.dimen.digital_x_offset);
+//            float textSize = resources.getDimension(isRound
+//                    ? R.dimen.digital_text_size_round : R.dimen.digital_text_size);
 
-            mTimePaint.setTextSize(textSize);
-            mDatePaint.setTextSize(textSize);
-            mHighTempPaint.setTextSize(textSize);
-            mLowTempPaint.setTextSize(textSize);
+            mDatePaint.setTextSize(resources.getDimension(R.dimen.date_text_size));
+            mDatePaint.setTextAlign(Paint.Align.CENTER);
+            mTimePaint.setTextSize(resources.getDimension(R.dimen.digital_watch_text_size));
+            mTimePaint.setTextAlign(Paint.Align.CENTER);
+            mHighTempPaint.setTextSize(resources.getDimension(R.dimen.temp_text_size));
+            mLowTempPaint.setTextSize(resources.getDimension(R.dimen.temp_text_size));
         }
 
         @Override
@@ -280,47 +284,62 @@ public class DigitalWatchFace extends CanvasWatchFaceService {
         @Override
         public void onAmbientModeChanged(boolean inAmbientMode) {
             super.onAmbientModeChanged(inAmbientMode);
-            Log.d(LOG_TAG, String.valueOf(inAmbientMode));
+            Log.d(LOG_TAG, "is we ambient?" + String.valueOf(inAmbientMode));
+            int opacityVal;
+//            int oppOpacityVal;
             if (mAmbient != inAmbientMode) {
+                opacityVal = AMBIENT_ALPHA;
+//                oppOpacityVal = NORMAL_ALPHA;
                 mAmbient = inAmbientMode;
                 if (mLowBitAmbient) {
                     mTimePaint.setAntiAlias(!inAmbientMode);
+                    mHighTempPaint.setAntiAlias(!inAmbientMode);
+                    mLowTempPaint.setAntiAlias(!inAmbientMode);
                 }
                 invalidate();
+            } else {
+                opacityVal = NORMAL_ALPHA;
+//                oppOpacityVal = AMBIENT_ALPHA;
             }
 
-//            TODO make all paints not do anti-aliasing
-            // Whether the timer should be running depends on whether we're visible (as well as
-            // whether we're in ambient mode), so we may need to start or stop the timer.
+//            mIconHider.setAlpha(oppOpacityVal);
+            mDatePaint.setAlpha(opacityVal);
+            mHighTempPaint.setAlpha(opacityVal);
+            mLowTempPaint.setAlpha(opacityVal);
+            // TODO: Figure out how to hide bitmaps...
             updateTimer();
         }
 
-        /**
-         * Captures tap event (and tap type) and toggles the background color if the user finishes
-         * a tap.
-         */
-        @Override
-        public void onTapCommand(int tapType, int x, int y, long eventTime) {
-            Resources resources = DigitalWatchFace.this.getResources();
-            switch (tapType) {
-                case TAP_TYPE_TOUCH:
-                    // The user has started touching the screen.
-                    break;
-                case TAP_TYPE_TOUCH_CANCEL:
-                    // The user has started a different gesture or otherwise cancelled the tap.
-                    break;
-                case TAP_TYPE_TAP:
-                    // The user has completed the tap gesture.
-                    mTapCount++;
-                    mBackgroundPaint.setColor(resources.getColor(mTapCount % 2 == 0 ?
-                            R.color.background : R.color.background2));
-                    break;
-            }
-            invalidate();
-        }
+//        /**
+//         * Captures tap event (and tap type) and toggles the background color if the user finishes
+//         * a tap.
+//         */
+//        @Override
+//        public void onTapCommand(int tapType, int x, int y, long eventTime) {
+//            Resources resources = DigitalWatchFace.this.getResources();
+//            switch (tapType) {
+//                case TAP_TYPE_TOUCH:
+//                    // The user has started touching the screen.
+//                    break;
+//                case TAP_TYPE_TOUCH_CANCEL:
+//                    // The user has started a different gesture or otherwise cancelled the tap.
+//                    break;
+//                case TAP_TYPE_TAP:
+//                    // The user has completed the tap gesture.
+//                    mTapCount++;
+//                    mBackgroundPaint.setColor(resources.getColor(mTapCount % 2 == 0 ?
+//                            R.color.background : R.color.background2));
+//                    break;
+//            }
+//            invalidate();
+//        }
 
         @Override
         public void onDraw(Canvas canvas, Rect bounds) {
+            int dateOffsMult = 4;
+            int tempOffsMult = dateOffsMult + 1;
+            int xCenterPos = (canvas.getWidth()) / 2;
+
             // Draw the background.
             if (isInAmbientMode()) {
                 canvas.drawColor(Color.BLACK);
@@ -333,19 +352,88 @@ public class DigitalWatchFace extends CanvasWatchFaceService {
             String text = mAmbient
                     ? String.format("%d:%02d", mTime.hour, mTime.minute)
                     : String.format("%d:%02d:%02d", mTime.hour, mTime.minute, mTime.second);
-            canvas.drawText(text, mXOffset, mYOffset, mTimePaint);
+            canvas.drawText(text,
+                    xCenterPos,
+                    DigitalWatchFace.this.getResources().getDimension(R.dimen.digital_watch_y_offset),
+                    mTimePaint);
 
             // draw things underneath clock
             if (mDateString != null) {
-                canvas.drawText(mDateString, mXOffset, mYOffset * 2, mDatePaint);
-                canvas.drawText(mHighTemp, mXOffset, mYOffset * 2, mHighTempPaint);
-                canvas.drawText(mLowTemp, mXOffset, mYOffset * 2, mLowTempPaint);
-            } else {
-                canvas.drawText("TEST", mXOffset, mYOffset * 2, mDatePaint);
-            }
+                canvas.drawText(mDateString,
+                        xCenterPos,
+                        DigitalWatchFace.this.getResources().getDimension(R.dimen.date_y_offset),
+                        mDatePaint);
 
+                canvas.drawText(mHighTemp,
+                        xCenterPos,
+                        mYOffset * tempOffsMult,
+                        mHighTempPaint);
+
+                canvas.drawText(mLowTemp,
+                        xCenterPos + mLowTempPaint.measureText("00º"),
+                        mYOffset * tempOffsMult,
+                        mLowTempPaint);
+
+                mWeatherIconBitmap = bitmapResourceSetter(mWeatherId);
+                canvas.drawBitmap(bitmapResourceSetter(mWeatherId),
+                        mXOffset * 4,
+                        mYOffset * 4,
+                        null);
+
+                if (!mAmbient){
+                    mLowTempPaint.setAlpha(NORMAL_ALPHA / 3);
+                    mHighTempPaint.setAlpha(NORMAL_ALPHA);
+                    mDatePaint.setAlpha(NORMAL_ALPHA - 50);
+                }
+            } else {
+                canvas.drawText("Waiting for Sync",
+                        xCenterPos,
+                        DigitalWatchFace.this.getResources().getDimension(R.dimen.date_y_offset),
+                        mDatePaint);
+                mDatePaint.setAlpha(NORMAL_ALPHA/3);
+                canvas.drawText("--º",
+                        xCenterPos,
+                        mYOffset * tempOffsMult,
+                        mHighTempPaint);
+                mHighTempPaint.setAlpha(NORMAL_ALPHA / 5);
+                canvas.drawText("--º",
+                        xCenterPos + mLowTempPaint.measureText("00º"),
+                        mYOffset * tempOffsMult,
+                        mLowTempPaint);
+                mLowTempPaint.setAlpha(NORMAL_ALPHA / 3);
+                mWeatherIconBitmap = bitmapResourceSetter(0);;
+                canvas.drawBitmap(bitmapResourceSetter(0),
+                        mXOffset * 3,
+                        mYOffset * 4,
+                        null);
+//                canvas.drawRect(
+//                        mXOffset * 3,
+//                        mYOffset * 4,
+//                        dpToPx(80),
+//                        dpToPx(80),
+//                        mIconHider);
+            }
         }
 
+        private Bitmap bitmapResourceSetter(int weatherId){
+            int resId = WearUtility.getIconResourceForWeatherCondition(weatherId);
+            String resString;
+            if (resId == -1){
+                resString = "ic_status";
+            }else{
+                resString = getResources().getResourceEntryName(resId);
+            }
+            int drawId = getResources().getIdentifier(resString, "drawable", getPackageName());
+            Drawable weatherIconDrawable = getResources().getDrawable(drawId, null);
+            return ((BitmapDrawable) weatherIconDrawable).getBitmap();
+        }
+
+
+        public int dpToPx(int dp) {
+            DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+            int px = Math.round(dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
+            return px;
+        }
         /**
          * Starts the {@link #mUpdateTimeHandler} timer if it should be running and isn't currently
          * or stops it if it shouldn't be running but currently is.
@@ -383,7 +471,6 @@ public class DigitalWatchFace extends CanvasWatchFaceService {
             mHighTemp = highTemp;
             mLowTemp = lowTemp;
             mWeatherId = weatherId;
-            int weatherResourceId = WearUtility.getIconResourceForWeatherCondition(weatherId);
         }
 
         @Override
@@ -411,7 +498,6 @@ public class DigitalWatchFace extends CanvasWatchFaceService {
                 String highTemp = bundle.getString(WEAR_HIGH_TEMP_KEY);
                 String lowTemp = bundle.getString(WEAR_LOW_TEMP_KEY);
                 int weatherId = bundle.getInt(WEAR_WEATHER_IMAGE_KEY);
-                int weatherResourceId = WearUtility.getIconResourceForWeatherCondition(weatherId);
                 updateUIElements(dateString, highTemp, lowTemp, weatherId);
             }
         }
